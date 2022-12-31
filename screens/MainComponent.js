6c07c4382c10fe01971e178746db0e20a2924562
Jon Honhart <jmhonhart@gmail.com>
2022-12-31 10:36:14 -0800

    Week 4 | Workshop Task 3

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -240,18 +240,17 @@ const Main = () => {
     }, [dispatch]);
 
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
-            Platform.OS === 'ios'
-                ? Alert.alert(
-                    'Initial Network Connectivity Type:',
-                    connectionInfo.type
-                )
-                : ToastAndroid.show(
-                    'Initial Network Connectivity Type: ' +
-                    connectionInfo.type,
-                    ToastAndroid.LONG
-                );
-        });
+        showNetInfo();
+    }, []);
+
+    const showNetInfo = async () => {
+        const connectionInfo = await NetInfo.fetch();
+        Platform.OS === "ios"
+            ? Alert.alert("Initial Network Connectivity Type:", connectionInfo.type)
+            : ToastAndroid.show(
+                "Initial Network Connectivity Type: " + connectionInfo.type,
+                ToastAndroid.LONG
+            );
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
@@ -260,7 +259,7 @@ const Main = () => {
         );
 
         return unsubscribeNetInfo;
-    }, []);
+    };
 
     const handleConnectivityChange = (connectionInfo) => {
         let connectionMsg = 'You are now connected to an active network.';
